7d082365f446e91f938819ec9893b36fe089b062
Dennis Felsing <dennis@felsing.org>
2026-08-15 15:32:32 +0200

    server: Reset snapshots on timeout
    A connection resumed by timeout protection can use a different protocol
    version than the connection that timed out. The snapshots kept in that
    slot are then used as delta base for the other protocol, where the same
    item type can have a different size, which can crash the server.
    
    (cherry picked from commit ed2b08f5d944bf84c1309989187fa886cc8a70db)

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -4998,6 +4998,11 @@ bool CServer::SetTimedOut(int ClientId, int OrigId)
 	m_NetServer.ResumeOldConnection(ClientId, OrigId);
 
 	m_aClients[ClientId].m_Sixup = m_aClients[OrigId].m_Sixup;
+	// This slot keeps playing with the resumed connection, which can use the other
+	// protocol version, so its snapshots must not be used as delta base anymore.
+	m_aClients[ClientId].m_Snapshots.PurgeAll();
+	m_aClients[ClientId].m_LastAckedSnapshot = -1;
+
 	const EClientBrand OrigClientBrand = m_aClients[OrigId].m_ClientBrand;
 
 	DelClientCallback(OrigId, "Timeout Protection used", this);
